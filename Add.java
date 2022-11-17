842891152e6973ad5e664b4de2c500fd95eebe8e
Adarshsoni25 <adarshsoni2503@gmail.com>
2022-11-18 00:33:05 +0530

    done

---
+++ Add.java
@@ -0,0 +1,14 @@
+class Add {
+
+  public static void main(String[] args) {
+    
+    System.out.println("sum of two numbers ");
+    int a = 10;
+    int b = 20;
+    
+    System.out.println(a + " & " + b);
+
+    int sum = a + b;
+    System.out.println("The sum is: " + sum);
+  }
+}
